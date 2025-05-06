482c59afef3a86664d54f6da2b50d8344301772f
Shuhei Yoshida <be.imaginative.oct.26@gmail.com>
2025-05-06 11:37:05 +0900

    Refactor error handling and improve debug logging in extractPdfInfo function

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -6,7 +6,7 @@ import "dotenv/config";
 
 let apiKey = process.env.OPENAI_API_KEY;
 if (!apiKey) {
-  console.error(`APIキーが設定されていません。処理を終了します。`)
+  console.error(`APIキーが設定されていません。処理を終了します。`);
   process.exit(1);
 }
 const client = new OpenAI({ apiKey: apiKey });
@@ -34,11 +34,13 @@ JSON形式
 - 金額...コンマなしの半角数字のみ。存在しない場合は0を出力。
 
 ## 注意点
-- ${config.myCompany.name}${config.myCompany.alias.length > 0
+- ${config.myCompany.name}${
+  config.myCompany.alias.length > 0
     ? "若しくは" + config.myCompany.alias.join(",")
     : ""
-  }は自社の名前の為、取引先名には使用しない。
+}は自社の名前の為、取引先名には使用しない。
 - 生成する文字列はjavascriptのJSON.parse()でパース可能な形式にしてください。
+- プロパティ名もダブルクォートで囲むようにしてください。
 - コードブロックは不要です。
 
 ## 取引先リスト
@@ -52,13 +54,16 @@ ${documentType}
  * PDFファイルから書類情報(日付、取引先名、証憑種別、金額)を抽出する
  * @param path PDFファイルのパス
  */
-export async function extractPdfInfo(pdfPath: string, options: CommandLineOption) {
+export async function extractPdfInfo(
+  pdfPath: string,
+  options: CommandLineOption
+) {
   // pdf -> base64
   const filename = path.basename(pdfPath);
   const data = fs.readFileSync(pdfPath);
   const base64String = data.toString("base64");
   // ファイル名を生成
-  const response = await client.responses.create({
+  const body: OpenAI.Responses.ResponseCreateParamsNonStreaming = {
     model: options.model || config.model,
     input: [
       {
@@ -76,9 +81,16 @@ export async function extractPdfInfo(pdfPath: string, options: CommandLineOption
         ],
       },
     ],
-  });
+  };
+  if (options.debug) {
+    const debugBody = JSON.parse(JSON.stringify(body));
+    debugBody.input[0].content[0].file_data = "[base64省略]";
+    console.log(`Request body`);
+    console.dir(debugBody, { depth: null });
+  }
+  const response = await client.responses.create(body);
   const responseOutputText = response.output_text;
-  // console.log(responseOutputText);
+  if (options.debug) console.log(`response.output_text: ${responseOutputText}`);
   const output = JSON.parse(responseOutputText) as ExtractPdfInfoOutput;
   // 未登録の取引先名と証憑種別を抽出
   const unregistered: Unregistered = { partner: "", documentType: "" };
